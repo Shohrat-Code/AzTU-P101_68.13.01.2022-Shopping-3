308c3ce308d2c76534eb0c3ca8324198e70a6dfc
Shohrat-Code <shohrat@code.edu.az>
2022-01-14 17:43:01 +0400

    Codes

---
+++ Azen/Azen/ViewModels/VmProduct.cs
@@ -0,0 +1,19 @@
+﻿using Azen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Azen.ViewModels
+{
+    public class VmProduct
+    {
+        public List<Product> Products { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+        public int PageCount { get; set; }
+        public double ItemCount { get; set; }
+        public int Page { get; set; }
+        public int? prdId { get; set; }
+        public string searchData { get; set; }
+    }
+}
